7503fae3c708078bb17aaf7aa80e840971cf7046
vincentlesc <vincent.leschaeve@gmail.com>
2019-07-27 14:21:31 +0200

    Create Security controller for login.

---
+++ src/Controller/Security/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/api/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+
+        return $this->json([
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
