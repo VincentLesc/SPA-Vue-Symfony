f35ec2042e076667a123f19ae03f014387e3d2c8
vincentlesc <vincent.leschaeve@gmail.com>
2019-07-27 11:19:17 +0200

    Grid system for blog index page.

--- assets/vue/views/blog/BlogIndex.vue
+++ assets/vue/views/blog/BlogIndex.vue
@@ -1,12 +1,21 @@
 <template>
-    <v-layout
-            align-center
-            justify-center
+    <v-container
+            fluid
+            grid-list-md
     >
-        <v-flex shrink>
-            <h1>Mes articles de blog</h1>
-
-            <section v-if="hasError">
+        <v-layout
+                align-center
+                justify-center
+        >
+            <h1 class="mb-5">Mes articles de blog</h1>
+        </v-layout>
+        <section
+                v-if="hasError"
+        >
+            <v-layout
+                    justify-center
+                    align-center
+            >
                 <v-alert
                         border="left"
                         color="red"
@@ -15,21 +24,34 @@
                     <v-icon>error</v-icon>
                     Oupss...Ce service est indisponible pour le moment
                 </v-alert>
-            </section>
-            <section v-else>
-                <div v-if="loading">
-                    <v-progress-linear
-                            indeterminate
-                            color="cyan"
-                    ></v-progress-linear>
-                </div>
-                <div v-for="post in posts">
-                    <h3>{{post.title}}</h3>
-                    <p>{{post.content}}</p>
-                </div>
-            </section>
-        </v-flex>
-    </v-layout>
+            </v-layout>
+        </section>
+        <section v-else>
+            <v-layout wrap justify-content-between>
+                <v-progress-linear
+                        v-if="loading"
+                        indeterminate
+                        color="cyan"
+                        xs6
+                ></v-progress-linear>
+                <v-flex
+                        v-for="post in posts"
+                        :key="post.id"
+                        d-flex
+                        md4
+                        xs12
+                        class="pa-1"
+                >
+                    <v-card
+                            class="pa-3"
+                    >
+                        <h3 class="mb-5">{{post.title}}</h3>
+                        <p>{{post.content}}</p>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </section>
+    </v-container>
 </template>
 <script>
     import PostAPI from '../../api/post';
