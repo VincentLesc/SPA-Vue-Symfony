4abce46759439a3c21ba9fb6505ea794c4b7c429
vincentlesc <vincent.leschaeve@gmail.com>
2019-08-04 09:10:09 +0200

    Create the route to acces user profile edition page.

--- assets/vue/router/index.js
+++ assets/vue/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Blog from '../views/blog/BlogIndex';
 import BlogNew from '../views/blog/BlogNew';
+import ProfileUser from "../views/profile/ProfileUser";
 import SecurityLogin from '../views/security/Login';
 import SecurityRegister from '../views/security/Register';
 import Home from '../views/Home';
@@ -14,6 +15,7 @@ export default new VueRouter({
     routes: [
         { path: '/blog', component: Blog},
         { path: '/blog/new', component: BlogNew},
+        { path: '/user/profile', component: ProfileUser},
         { path: '/login', component: SecurityLogin},
         { path: '/register', component: SecurityRegister},
         { path: '/*', component: Home}
