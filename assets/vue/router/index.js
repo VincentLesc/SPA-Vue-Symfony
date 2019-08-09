39950ab5cbdca1e7d6ffabe71cf74778de1a28cb
VincentLesc <vincent.leschaeve@gmail.com>
2019-08-09 15:01:50 +0200

    Control if is connected befor profile access

--- assets/vue/router/index.js
+++ assets/vue/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store';
 import Blog from '../views/blog/BlogIndex';
 import BlogNew from '../views/blog/BlogNew';
 import ProfileUser from "../views/profile/ProfileUser";
@@ -10,14 +11,30 @@ import Home from '../views/Home';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+let router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/blog', component: Blog},
         { path: '/blog/new', component: BlogNew},
-        { path: '/user/profile', component: ProfileUser},
+        { path: '/user/profile', component: ProfileUser, meta: {requiresAuth: true}},
         { path: '/login', component: SecurityLogin},
         { path: '/register', component: SecurityRegister},
         { path: '/*', component: Home}
-    ]
+    ],
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters['security/isAuthenticated']) {
+            next();
+        } else {
+            next({
+                path: '/login'
+            })
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
